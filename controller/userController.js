5caa7f816813c1c1cf019b5c0672f8b864d9e10f
SUBHAM SARKAR <127990430+PROPHETCYCO@users.noreply.github.com>
2024-09-06 18:37:48 +0530

    final

--- controller/userController.js
+++ controller/userController.js
@@ -55,7 +55,7 @@ export const findCoins = async (req, res) => {
             return res.status(404).json({message: "User not found"});
         }
 
-        res.status(200).json(userFound.coins);
+        res.status(200).json({coins: userFound.coins});
     } catch (error) {
         res.status(500).json({error: error.message});
     }
