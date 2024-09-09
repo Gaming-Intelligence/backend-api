89f8fc0c8e3bbd81bc58b0408075aa64b0f77817
Ankit kumar <ankit.dev.78588@gmail.com>
2024-09-09 19:05:34 +0530

    API's modified

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,6 @@ const route = express.Router();
 route.post("/saveCoins", saveCoins);
 route.post("/saveUser", saveData);
 route.post("/findCoins", findCoins);
-route.post("/registerUsingLink", registerUsingLink);
 
 
 export default route;
